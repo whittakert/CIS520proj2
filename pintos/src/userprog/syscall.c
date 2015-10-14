a3d0e13c92288a5e541fe78ec2558f2f8abf96ce
evk12 <evk12@yahoo.com>
2015-10-13 20:38:42 -0500

    Doug changed syscall

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -52,6 +52,9 @@ int arg_1 = *((int*)f->esp + 1);
 int arg_2 = *((int*)f->esp + 2);
 int arg_3 = *((int*)f->esp + 3);
 
+void* krnl_addr;
+char* krnl_str;
+
 switch( syscall_num )
 {
 	case SYS_HALT:
@@ -208,30 +211,44 @@ sys_exit (int exit_code)
 static int
 sys_exec (const char *ufile) 
 {
-/* Add code */
-  thread_exit ();
+	int tid;
+	lock_acquire (&fs_lock);
+	tid = process_execute (ufile);
+	lock_release (&fs_lock);
+	return tid;
 }
  
 /* Wait system call. */
 static int
 sys_wait (tid_t child) 
 {
-/* Add code */
-  thread_exit ();
+	int status;
+	lock_acquire (&fs_lock);
+	status = process_wait(child);
+	lock_release (&fs_lock);
+	return status;
 }
  
 /* Create system call. */
 static int
 sys_create (const char *ufile, unsigned initial_size) 
 {
-  return 0;
+	bool created;
+	lock_acquire (&fs_lock);
+  created = filesys_create(ufile, (off_t)initial_size);
+  lock_release (&fs_lock);
+	return created;
 }
  
 /* Remove system call. */
 static int
 sys_remove (const char *ufile) 
 {
-/* Add code */
+	bool removed;
+	lock_acquire (&fs_lock);
+  removed = filesys_remove(ufile);
+  lock_release (&fs_lock);
+	return removed;
 }
  
 /* A file descriptor, for binding a file handle to a file. */
@@ -275,24 +292,90 @@ sys_open (const char *ufile)
 static struct file_descriptor *
 lookup_fd (int handle)
 {
-/* Add code to lookup file descriptor in the current thread's fds */
-  thread_exit ();
+	struct list* file_list = &(thread_current()->fds);
+	struct list_elem* temp;
+	temp = list_head ( file_list );
+	while ( temp != list_end( file_list ) )
+	{
+		struct file_descriptor *fd = list_entry(temp, struct file_descriptor, elem);
+		if ( fd->handle == handle )
+		{
+			return fd;
+		}
+		else
+		temp = list_next ( temp ); 
+	}
+	process_exit(); // Either or?
+	thread_exit();
 }
  
 /* Filesize system call. */
 static int
 sys_filesize (int handle) 
 {
-/* Add code */
-  thread_exit ();
+	int size;
+	struct file_descriptor *fd = lookup_fd (handle);
+  lock_acquire (&fs_lock);
+	size= file_length(fd->file);
+  lock_release (&fs_lock);
+	return size;
 }
  
 /* Read system call. */
 static int
 sys_read (int handle, void *udst_, unsigned size) 
 {
-/* Add code */
-  thread_exit ();
+  uint8_t *udst = udst_;
+  struct file_descriptor *fd = NULL;
+  int bytes_read = 0;
+
+  /* Lookup up file descriptor. */
+  if (handle != STDOUT_FILENO)
+    fd = lookup_fd (handle);
+
+  lock_acquire (&fs_lock);
+  while (size > 0) 
+    {
+      /* How much bytes to read from this page? */
+      size_t page_left = PGSIZE - pg_ofs (udst);
+      size_t read_amt = size < page_left ? size : page_left;
+      off_t retval;
+
+      /* Check that we can touch this user page. */
+      if (!verify_user (udst)) 
+        {
+          lock_release (&fs_lock);
+          thread_exit ();
+        }
+
+			
+      /* Do the write. 
+      if (handle == STDOUT_FILENO)
+        {
+          putbuf (usrc, write_amt);
+          retval = write_amt;
+        }
+      else
+			*/
+      	retval = file_read (fd->file, udst, read_amt);
+      if (retval < 0) 
+        {
+          if (bytes_read == 0)
+            bytes_read = -1;
+          break;
+        }
+      bytes_read += retval;
+
+      /* If it was a short write we're done. */
+      if (retval != (off_t) read_amt)
+        break;
+
+      /* Advance. */
+      udst += retval;
+      size -= retval;
+    }
+  lock_release (&fs_lock);
+  return bytes_read;
 }
  
 /* Write system call. */
@@ -325,7 +408,7 @@ sys_write (int handle, void *usrc_, unsigned size)
       /* Do the write. */
       if (handle == STDOUT_FILENO)
         {
-          putbuf (usrc, write_amt);
+          putbuf ((char*)usrc, write_amt);
           retval = write_amt;
         }
       else
@@ -355,24 +438,38 @@ sys_write (int handle, void *usrc_, unsigned size)
 static int
 sys_seek (int handle, unsigned position) 
 {
-/* Add code */
-  thread_exit ();
+	struct file_descriptor *fd = lookup_fd (handle);
+	if (fd == NULL)
+		sys_exit(-1); //TW 10.12
+	lock_acquire (&fs_lock);
+	file_seek(fd->file, position);
+	lock_release (&fs_lock);
 }
  
 /* Tell system call. */
 static int
 sys_tell (int handle) 
 {
-/* Add code */
-  thread_exit ();
+	struct file_descriptor *fd = lookup_fd (handle);
+	if (fd == NULL)
+		sys_exit(-1); //TW 10.12	
+	unsigned pos;
+	lock_acquire (&fs_lock);
+	pos = (unsigned)file_tell(fd->file);
+	lock_release (&fs_lock);
+	return pos;
 }
  
 /* Close system call. */
 static int
 sys_close (int handle) 
 {
-/* Add code */
-  thread_exit ();
+	struct file_descriptor *fd = lookup_fd (handle);
+	if (fd == NULL)
+		sys_exit(-1);//TW 10.12 
+	lock_acquire (&fs_lock);
+	file_close(fd->file);
+	lock_release (&fs_lock);
 }
  
 /* On thread exit, close all open files. */
